5dab669cb9c70bd1f7964098c9231b6bc1c1b077
yamamotoshinji <perfectyear0323@gmail.com>
2024-10-14 20:37:10 +0900

    form修正

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -1,78 +1,79 @@
 @extends('layouts.app')
 
 @section('css')
-<link rel="stylesheet" href="{{ asset('css/shop_all.css')}}">
+<link rel="stylesheet" href="{{ asset('css/index.css')}}">
 @endsection
 
 @section('search')
+    <div class="search">
+        <form class="search__form" action="/search" method="get">
+            <label class="search-label">
+                <select class="search__select" name="region">
+                    <option value="">All area</option>
+                    @foreach ($regions as $region)
+                    <option class="select-box__option" value="{{ $region }}" {{ request('region') == $region ? 'selected' : '' }}>{{ $region }} </option>
+                    @endforeach
+                </select>
+            </label>
 
-<div class="search">
-    <form action="/search" method="get">
-        <label class="search-label">
-            <select class="search__select" name="region">
-                <option value="">All area</option>
-                @foreach ($regions as $region)
-                <option class="select-box__option" value="{{ $region }}" {{ request('region') == $region ? 'selected' : '' }}>{{ $region }} </option>
-                @endforeach
-            </select>
-        </label>
+            <label class="search-label">
+                <select class="search__select" name="genre">
+                    <option value="">All genre</option>
+                    @foreach ($genres as $genre)
+                    <option value="{{ $genre }}" {{ request('genre') == $genre ? 'selected' : '' }}>
+                    {{ $genre }}</option>
+                    @endforeach
+                </select>
+            </label>
 
-        <label class="search-label">
-            <select class="search__select" name="genre">
-                <option value="">All genre</option>
-                @foreach ($genres as $genre)
-                <option value="{{ $genre }}" {{ request('genre') == $genre ? 'selected' : '' }}>
-                {{ $genre }}</option>
-                @endforeach
-            </select>
-        </label>
-
-        <div class="search__item">
-            <input type="text" name="store_name" placeholder="Search..." />
-            <button type="submit"></button>
-        </div>
-    </form>
-</div>
+            <div class="search__item">
+                <div class="search__item-button"></div>
+                <label class="search__item-label">
+                    <input type="text" name="store_name" class="search__item-input" placeholder="Search..." />
+                </label>
+            </div>
+        </form>
+    </div>
 
 @endsection
 
 @section('content')
-<div class="list">
-    @foreach($shops as $shop)
-    <div class="shop">
-        <img class="shop__image" src="{{ $shop->image_url }}" alt="イメージ画像" >
-        <div class="shop__content">
-            <p class="shop__title">{{$shop['store_name']}}</p>
-            <div class="shop__tag">
-                <p>#{{$shop->region}}</p>
-                <P>#{{$shop->genre}}</P>
-            </div>
-            <div class="shop__item">
-                <button class="shop__button" onclick="location.href='{{ route('detail',['id' => $shop['id'] ]) }}'">詳しく見る</button>
-                <form class="shop__favorite" action="/favorite" method="get">
-                @csrf
-                    <input type="hidden" name="shop_id" value="{{ $shop['id'] }}" />
-                    <input type="hidden" name="page" value="shop_all" />
-                @php
-                    $favored = false;
-                @endphp
-                @foreach($favorites as $favorite)
-                    @if($shop['id'] == $favorite['shop_id'])
-                        @php
-                            $favored = true;
-                            break;
-                        @endphp
+    <div class="list">
+        @foreach($shops as $shop)
+        <div class="shop">
+            <img class="shop__image" src="{{ $shop->image_url }}" alt="イメージ画像" >
+            <div class="shop__content">
+                <p class="shop__title">{{$shop['store_name']}}</p>
+                <div class="shop__tag">
+                    <p>#{{$shop->region}}</p>
+                    <P>#{{$shop->genre}}</P>
+                </div>
+                <div class="shop__item">
+                    <button class="shop__button" onclick="location.href='{{ route('detail',['id' => $shop['id'] ]) }}'">詳しく見る</button>
+                    <form class="shop__favorite" action="/favorite" method="get">
+                    @csrf
+                        <input type="hidden" name="shop_id" value="{{ $shop['id'] }}" />
+                        <input type="hidden" name="page" value="shop_all" />
+                    @php
+                        $favored = false;
+                    @endphp
+                    @foreach($favorites as $favorite)
+                        @if($shop['id'] == $favorite['shop_id'])
+                            @php
+                                $favored = true;
+                                break;
+                            @endphp
+                        @endif
+                    @endforeach
+                    @if($favored)
+                        <button class="heart__favorite"></button>
+                    @else
+                        <button class="heart"></button>
                     @endif
-                @endforeach
-                @if($favored)
-                    <button class="heart__favorite"></button>
-                @else
-                    <button class="heart"></button>
-                @endif
-                </form>
+                    </form>
+                </div>
             </div>
         </div>
+        @endforeach
     </div>
-    @endforeach
-</div>
 @endsection
